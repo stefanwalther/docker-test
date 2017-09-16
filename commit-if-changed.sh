5e1fd65bb41dae0edd374a3695e1e42105946a65
Stefan Walther <stefanwalther@users.noreply.github.com>
2017-09-16 14:00:36 +0200

    test prepush hook

--- commit-if-changed.sh
+++ commit-if-changed.sh
@@ -4,5 +4,10 @@ if (git status | grep "README.md" -q);
 then
   echo "OK, README has changed";
   git add ./README.md;
+
+  git status;
+  echo "do the commit ...";
   git commit ./README.md -m "docs: update README.md";
+
+  git status;
 fi
