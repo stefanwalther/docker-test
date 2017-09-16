3d950cb860e4e1a89c5c6d6c19e7a426834ecf3e
Stefan Walther <stefanwalther@users.noreply.github.com>
2017-09-16 13:53:26 +0200

    ci: some changes to test prepush

--- commit-if-changed.sh
+++ commit-if-changed.sh
@@ -1,6 +1,8 @@
 #!/usr/bin/env bash
 
-if (git status | grep "../README.md");
+if (git status | grep "README.md" -q);
 then
   echo "OK, README has changed";
+  git add ./README.md;
+  git commit ./README.md -m "docs: update README.md";
 fi
