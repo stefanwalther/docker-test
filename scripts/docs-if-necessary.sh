be30857dd7e1cf06e59843379b370d9e4f9599fe
Stefan Walther <stefanwalther@users.noreply.github.com>
2017-09-18 20:44:34 +0200

    docs: some changes in verb

---
+++ scripts/docs-if-necessary.sh
@@ -0,0 +1,24 @@
+#!/usr/bin/env bash
+
+if (git status | grep -E "docs/|.verb.md" -q);
+then
+  npm run docs;
+  exit 10;
+
+#  if (git status | grep "README.md" -q);
+#  then
+#    echo "OK, README has changed";
+#    git add ./README.md;
+#
+#    git status;
+#    echo "do the commit ...";
+#    git commit ./README.md -m "docs: update README.md";
+#    git push origin master;
+#
+#    echo "--";
+#    echo "GIT STATUS:";
+#    git status;
+#    echo "--";
+#  fi
+
+fi
