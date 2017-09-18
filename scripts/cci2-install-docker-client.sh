8e6f195466e2dcb4c270fd0730e605ea85cf0c7c
Stefan Walther <stefanwalther@users.noreply.github.com>
2017-09-18 21:05:52 +0200

    ci: use edge download for docker images

--- scripts/cci2-install-docker-client.sh
+++ scripts/cci2-install-docker-client.sh
@@ -1,6 +1,10 @@
 #!/bin/bash
 set -e
 VER=$1
-curl -L -o /tmp/docker-$VER.tgz https://get.docker.com/builds/Linux/x86_64/docker-$VER.tgz
+# See list of builds:
+#   stable: https://download.docker.com/linux/static/stable/x86_64/
+#   edge: https://download.docker.com/linux/static/edge/x86_64/
+#curl -L -o /tmp/docker-$VER.tgz https://get.docker.com/builds/Linux/x86_64/docker-$VER.tgz
+curl -L -o /tmp/docker-$VER.tgz https://download.docker.com/linux/static/edge/x86_64/docker-$VER.tgz
 tar -xz -C /tmp -f /tmp/docker-$VER.tgz
 mv /tmp/docker/* /usr/bin
